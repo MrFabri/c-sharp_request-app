8b7484748a1d1e688f9bae7c95d7e4d47af95024
Mr Fabri <mrfabri02@gmail.com>
2020-11-18 18:45:39 +0100

    upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Simple terminal app
+
+using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace getrequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+
+            var data = client.DownloadString(API_URL);
+            dynamic posts = JsonConvert.DeserializeObject(data);
+
+            foreach(var post in posts) {
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+        }
+    }
+}
